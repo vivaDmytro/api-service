3e5f764d66bfea2c1df25d018748ed80a8060dbd
Dmytro Budnik <dmytro@budnik.local>
2025-08-31 11:27:29 +0200

    cleanup

--- src/main/java/org/dvb/client/WorkerClient.java
+++ src/main/java/org/dvb/client/WorkerClient.java
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(
-        name = "storage-service",
-        url = "${storage-service.url:http://localhost:8081}"
+        name = "worker-service",
+        url = "${worker-service.url:http://localhost:8081}"
 )
-public interface StorageClient {
+public interface WorkerClient {
 
-    @GetMapping("/items/{id}")
+    @GetMapping("v1/items/{id}")
     Item getItem(@PathVariable("id") String id);
 
-    @PostMapping("/items")
+    @PostMapping("v1/items")
     Item createItem(@RequestBody Item item);
 }
--- src/main/java/org/dvb/service/ItemService.java
+++ src/main/java/org/dvb/service/ItemService.java
@@ -2,30 +2,36 @@ package org.dvb.service;
 
 import feign.FeignException;
 import lombok.RequiredArgsConstructor;
-import org.dvb.client.StorageClient;
+import lombok.extern.slf4j.Slf4j;
+import org.dvb.client.WorkerClient;
 import org.dvb.model.Item;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ItemService {
 
-    private final StorageClient storageClient;
+    private final WorkerClient workerClient;
 
     public Optional<Item> getItem(String itemId) {
         try {
-            return Optional.ofNullable(storageClient.getItem(itemId));
+            log.info("Getting item {}", itemId);
+            return Optional.ofNullable(workerClient.getItem(itemId));
         } catch (FeignException e) {
+            log.error("Error getting item {}", itemId, e);
             return Optional.empty();
         }
     }
 
     public Optional<Item> createItem(Item item) {
         try {
-            return Optional.ofNullable(storageClient.createItem(item));
+            log.info("Creating new item: {}", item);
+            return Optional.ofNullable(workerClient.createItem(item));
         } catch (FeignException e) {
+            log.error("Error creating new item: {}", item, e);
             return Optional.empty();
         }
     }
